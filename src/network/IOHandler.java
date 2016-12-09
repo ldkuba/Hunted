c3192827f4535376ba156289aeda1d238eb56570
Vakerrian <vakerrian@gmail.com>
2016-12-09 04:56:51 +0000

    Re-implemented host/server methods

--- src/network/IOHandler.java
+++ src/network/IOHandler.java
@@ -3,16 +3,54 @@ package network;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
+import java.net.*;
 
-public abstract class IOHandler {
+public class IOHandler {
+
 	private DataOutputStream outputStream;
+
 	private DataInputStream inputStream;
-	private Socket socket;
 
+	private Socket clientSocket;
 
+	private ServerSocket serverSocket;
 
-	public void send(String data) {
+	public void createServer(int port){
+		try {
+			serverSocket = new ServerSocket(port);
+			serverSocket.setSoTimeout(10000);
+
+			System.out.println("Server created on"  + InetAddress.getLocalHost() + ":" + serverSocket.getLocalPort());
+		} catch (SocketException e) {
+			System.err.println("Unable to use port: " + port);
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void runServer(){
+		try {
+			clientSocket = serverSocket.accept();
+			inputStream = new DataInputStream(clientSocket.getInputStream());
+			outputStream = new DataOutputStream(clientSocket.getOutputStream());
+		}catch(SocketTimeoutException s) {
+			System.out.println("Socket timed out");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String recieveData(){
+		try {
+			return inputStream.readUTF();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void sendData(String data) {
 		try {
 			outputStream.writeUTF(data);
 		}catch(IOException e) {
@@ -20,11 +58,14 @@ public abstract class IOHandler {
 		}
 	}
 
-	public void close() {
+	public void closeS() {
 		try {
-			socket.close();
+			clientSocket.close();
+			serverSocket.close();
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+
 }
